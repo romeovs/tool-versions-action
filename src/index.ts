e913cd44d72e0a96fe0e237c111f0f8e50afab42
Romeo Van Snick <romeo@romeovansnick.be>
2024-01-27 16:11:23 +0100

    Add action code

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import { promises as fs } from "fs"
+import * as core from "@actions/core"
+
+try {
+	await run()
+} catch (err) {
+	if (err instanceof Error) {
+		core.setFailed(err.message)
+	} else {
+		throw err
+	}
+}
+
+async function run() {
+	const path = core.getInput("path")
+
+	core.debug(`Loading versions from ${path}`)
+	const tools = await read(path)
+
+	for (const tool of tools) {
+		core.debug(`version for ${tool.name}: ${tool.version}`)
+		core.setOutput(tool.name, tool.version)
+	}
+}
+
+type Tool = {
+	name: string
+	version: string
+}
+
+async function read(path: string): Promise<Tool[]> {
+	const content = await fs.readFile(path, "utf-8")
+	return content.split("\n").map(function (line) {
+		const [name, version] = line.trim().split("s+")
+		if (!name || !version) {
+			throw new Error(`Cannot parse line in ${path}: ${line}`)
+		}
+		return {
+			name: name.trim(),
+			version: version.trim(),
+		}
+	})
+}
